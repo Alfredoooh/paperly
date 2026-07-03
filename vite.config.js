8fb96b72264e0b6afb01e2500c48d0c7577070cd
Alfredoooh <alfredopjonas@gmail.com>
2026-07-03 08:38:08 +0100

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { resolve } from 'path';
 import { cpSync, mkdirSync, existsSync } from 'fs';
 
-const apps = ['auth', 'home', 'ai', 'music', 'games', 'media', 'news', 'downloader'];
+const apps = ['auth', 'home', 'ai', 'music', 'games', 'media', 'profilelens', 'downloader'];
 
 export default defineConfig({
   plugins: [
@@ -42,7 +42,7 @@ export default defineConfig({
         music: resolve(__dirname, 'src/music/index.html'),
         games: resolve(__dirname, 'src/games/index.html'),
         media: resolve(__dirname, 'src/media/index.html'),
-        news: resolve(__dirname, 'src/news/index.html'),
+        profilelens: resolve(__dirname, 'src/profilelens/index.html'),
         downloader: resolve(__dirname, 'src/downloader/index.html'),
       }
     }
